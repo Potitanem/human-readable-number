4d324ba92f68bbf9f5208bbef232b3fc7f42f24d
Denis <ya.denis300000@yandex.ru>
2023-01-10 15:14:58 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+	let numOfOnes = ['', 'one','two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+	'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+	'seventeen', 'eighteen', 'nineteen'];
+	let tenths = ['','','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
+	'ninety'];
+	let numberStr = String(number);
+	if(number == 0){
+		return 'zero';
+	}
+	if (number < 20){
+		return numOfOnes[number];
+	}
+	if (numberStr.length == 2){
+		if(numberStr[1] == '0'){
+			return tenths[numberStr[0]];
+		} else
+				return tenths[numberStr[0]] + ' ' + numOfOnes[numberStr[1]];
+	}
+	if(numberStr.length == 3){
+		if(numberStr[1] == '0' && numberStr[2] == '0'){
+			return numOfOnes[numberStr[0]] + ' hundred';
+		}
+		else
+			return numOfOnes[numberStr[0]] + ' hundred ' + toReadable(+(numberStr[1] + numberStr[2]));
+	}
 }
